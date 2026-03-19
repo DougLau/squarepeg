f85685ded4b90b2d503ed6b9db83cddf9960ba4c
Doug Lau <doug.p.lau@gmail.com>
2026-03-19 15:02:36 -0500

    Improved comments

--- src/peg.rs
+++ src/peg.rs
@@ -4,11 +4,13 @@
 //
 use std::fmt;
 
-/// A Peg identifies a partiion (tile) on a map grid at a specific zoom level.
+/// A Peg identifies a partition (tile) on a map grid at a specific zoom level.
 ///
-/// It uses XYZ addressing, with X increasing from west to east and Y increasing
-/// from north to south.  The X and Y values can range from 0 to
-/// 2<sup>Z</sup>-1.
+/// It uses XYZ addressing, with `X` increasing from west to east and `Y`
+/// increasing from north to south.  `Z` represents zoom, with `0` containing
+/// one tile for the entire Earth.  Each subsequent zoom level has twice as
+/// many tiles in each dimension (the `X` and `Y` values can range from `0` to
+/// `2<sup>Z</sup>-1`).
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Peg {
     x: u32, // not public to prevent invalid values being created
@@ -17,17 +19,17 @@ pub struct Peg {
 }
 
 impl Peg {
-    /// Get the X value.
+    /// Get the X value
     pub fn x(&self) -> u32 {
         self.x
     }
 
-    /// Get the Y value.
+    /// Get the Y value
     pub fn y(&self) -> u32 {
         self.y
     }
 
-    /// Get the Z (zoom) value.
+    /// Get the Z (zoom) value
     pub fn z(&self) -> u32 {
         self.z
     }
@@ -40,14 +42,14 @@ impl fmt::Display for Peg {
 }
 
 impl Peg {
-    /// Create a new Peg (partition of Earth's geography).
+    /// Create a new Peg (partition of Earth's geography)
     ///
     /// If invalid, returns `None`
     pub fn new(x: u32, y: u32, z: u32) -> Option<Self> {
         Peg::check_valid(x, y, z).then_some(Peg { x, y, z })
     }
 
-    /// Check whether XYZ is valid.
+    /// Check whether XYZ is valid
     fn check_valid(x: u32, y: u32, z: u32) -> bool {
         z <= 31 && {
             let s = 1 << z;
